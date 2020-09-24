7669b55782a77c96f879a69743bf91cd4c3e2d10
Peter <lplsz2000@gmail.com>
2020-09-24 18:12:41 +1000

    Add tests for auth_logout

--- src/auth_test.py
+++ src/auth_test.py
@@ -3,11 +3,17 @@ from error import InputError, AccessError
 import pytest
 
 @pytest.fixture
-def login_account():
+def register_new_account():
     return auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
 
+# Successful cases for auth_login
+def test_login_success_case():
+    result = register_new_account
+    auth_login('validemail@gmail.com', '123abc!@#')
+
+# Fail cases for auth_login
 def test_login_fail_case():
-    result = login_account
+    result = register_new_account
     with pytest.raises(InputError) as e:
         auth_login('didntusethis@gmail.com', '123abcd!@#')  # Never registered
         auth_login('validemail@gmail.com', '123')           # Wrong password
@@ -17,12 +23,21 @@ def test_login_invalid_email():
         auth_login('didntusethis@gmail', '123abcd!@#')
         auth_login('didntusethis.com', '123abcd!@#')
 
-def test_login_success_case():
-    result = login_account
-    auth_login('validemail@gmail.com', '123abc!@#')
+# Tests for auth_logout
+def test_logout_fail():
+    {u_id, token} = register_new_account
+    auth_logout(token)
+    {is_success} = auth_logout(token)
+    assert is_success = False
 
-# Successful cases for aut_register
+def test_logout_success():
+    {u_id, token} = register_new_account
+    {is_success} = auth_logout(token)
+    assert is_success = True
 
+# Successful cases for auth_register
+def test_register_success_case():
+    result = register_new_account
 
 # Fail cases for auth_register
 def auth_register_invalid_email():
@@ -31,7 +46,7 @@ def auth_register_invalid_email():
         auth_register('didntusethis.com', '123abcd!@#')
 
 def auth_register_used_email():
-    result = login_account
+    result = register_new_account
     with pytest.raises(InputError) as e:
         auth_register('validemail@gmail.com', '123abcd!@#', 'Peter', 'Li')
 
