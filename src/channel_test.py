2709a0f30966d83470d884a569b45acc395d6226
Mathieu PATUREL <australie.p@gmail.com>
2020-09-24 14:36:21 +1000

    write some very simple test for test_channel_a_and_b
    this is wishful coding, a lot of function haven't been implemented yet

---
+++ src/channel_test.py
@@ -0,0 +1,45 @@
+from database import data
+from auth import auth_register, auth_get_user_details_from_id
+from channel import channel_details
+from channels import channels_create
+
+
+def register_a_and_b():
+    """ Registers sample users """
+    paira = auth_register("email@a.com", "", "A", "LastA")
+    pairb = auth_register("email@b.com", "", "B", "LastB")
+    return paira, pairb
+
+
+def test_channel_details_invalid():
+    data.clear()
+    token = "token"  # TODO: make a proper token using auth
+
+    (ida, tokena), (idb, tokenb) = register_a_and_b()
+
+    id1 = channels_create(tokena, "channel1", False)
+    id2 = channels_create(tokenb, "channel2", False)
+
+    details1 = channel_details(tokena, id1)
+    assert details1 = {
+        'name': 'channel1',
+        'owner_members': [
+            auth_get_user_details_from_id(ida),
+        ],
+        'all_members': [
+            auth_get_user_details_from_id(ida),
+        ]
+    }
+    details2 = channel_details(tokena, id1)
+    assert details2 = {
+        'name': 'channel2',
+        'owner_members': [
+            auth_get_user_details_from_id(idb),
+        ],
+        'all_members': [
+            auth_get_user_details_from_id(idb),
+        ]
+    }
+
+# TODO: test_channel_details invalid token
+# TODO: test_channel_details unknwon channel
