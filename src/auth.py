a809490027437a9ee664537b27e666287321d9a3
Peter <lplsz2000@gmail.com>
2020-09-24 14:59:55 +1000

    Add check email function to auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,9 @@
+import re
+
 def auth_login(email, password):
+    try:
+
+
     return {
         'u_id': 1,
         'token': '12345',
@@ -14,3 +19,11 @@ def auth_register(email, password, name_first, name_last):
         'u_id': 1,
         'token': '12345',
     }
+
+# Helper function for validating an Email 
+def check_email(email):  
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex,email)):  
+        return True
+    else:  
+        return False
