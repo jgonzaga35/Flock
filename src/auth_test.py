614ea4b23ff8bc8cb0d4ee9d580ee6a36781d2fd
Peter <lplsz2000@gmail.com>
2020-09-24 16:07:01 +1000

    Fix some style

--- src/auth_test.py
+++ src/auth_test.py
@@ -5,18 +5,18 @@ import pytest
 def login_account():
     return auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
 
-def test_login_FailCase():
+def test_login_fail_case():
     result = login_account
     with pytest.raises(InputError) as e:
         auth_login('didntusethis@gmail.com', '123abcd!@#')  # Never registered
         auth_login('validemail@gmail.com', '123')           # Wrong password
 
-def test_login_InvalidEmail():
+def test_login_invalid_email():
     with pytest.raises(InputError) as e:
         auth_login('didntusethis@gmail', '123abcd!@#')
         auth_login('didntusethis.com', '123abcd!@#')
 
-def test_login_SuccessCase():
+def test_login_success_case():
     result = login_account
     auth.auth_login('validemail@gmail.com', '123abc!@#')
 
