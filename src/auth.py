c6cacbe9adf5591b85c422f62b2e96a4abc4e2c3
Mathieu PATUREL <australie.p@gmail.com>
2020-09-24 21:37:50 +1000

    fix warning when running pytest
    It's because of the regex thing to check emails. One letter fix :^)

--- src/auth.py
+++ src/auth.py
@@ -67,7 +67,7 @@ def highest_id():
 
 # Helper function for validating an Email 
 def check_email(email):  
-    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if(re.search(regex,email)):  
         return
     else:  
