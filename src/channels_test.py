2bd58a435531f678f48d4bf747bf57d3d7ee78d7
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-09-25 15:38:17 +1000

    added extra tests

---
+++ src/channels_test.py
@@ -0,0 +1,41 @@
+from channels import channels_create
+from database import database
+from error import InputError
+from auth import auth_register, auth_login
+import pytest
+
+def test_create_simple():
+    # clear database here
+    user = register_and_login_user()
+    channel = channels_create(user['token'], 'channel', is_public = True)
+    channel_id = channel['channel_id']
+    assert channel_id == 0
+
+def test_create_public():
+    # clear database here
+    user = register_and_login_user()
+    channel = channels_create(user['token'], 'channel', is_public = True)
+    public_status = channel['is_public']
+    assert public_status == True
+
+def test_create_simple_private():
+    # clear database here
+    user = register_and_login_user()
+    channel = channels_create(user['token'], 'channel', is_public = False)
+    channel_id = channel['channel_id']
+    assert channel_id == 0
+
+def test_long_name_error():
+    # clear database here
+    user = register_and_login_user()
+    with pytest.raises(InputError):
+        channels_create(user['token'], 'channel name longer than twenty char', is_public = True)
+
+
+
+# Helper function that registers a user and logs them in
+# Returns {u_id, token}
+def register_and_login_user():
+    user_01 = auth_register('validemail01@gmail.com', 'validpass@!01', 'First', 'User')
+    user_01_credentials = auth_login('validemail01@gmail.com', 'validpass@!01')
+    return user_01_credentials
