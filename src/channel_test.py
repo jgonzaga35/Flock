3265e00fd91f964a2fc89d4b36731f377872941e
Eric Bai <baixu.must@gmail.com>
2020-09-25 20:06:39 +1000

    add two moretests for channel_leave

--- src/channel_test.py
+++ src/channel_test.py
@@ -13,21 +13,21 @@ def new_user():
 
 # Join the channel successfully
 def test_channel_join_success(new_user):
-    u_id, token = new_user['u_id'], new_user['token']
-    channel_id = channels_create(token, "Channel_A", True)
-    channel_join(token, channel_id)
+    user = new_user
+    channel_id = channels_create(user['token'], "Channel_A", True)
+    channel_join(user['token'], channel_id)
 
 # user try to join a channel with invalid channel id
 def test_channel_join_invalid_channelId(new_user):
-    u_id, token = new_user['u_id'], new_user['token']
+    user = new_user
     invalid_channel_id = 1
     with pytest.raises(InputError):
-        channel_join(token, invalid_channel_id)
+        channel_join(user['token'], invalid_channel_id)
 
 # join a user without authority to a channel    
 def test_channel_join_user_not_auth(new_user):
-    u_id, token = new_user['u_id'], new_user['token']
-    channel_id = channels_create(token, "Private_channel", False) # Create a new private channel
+    user_A = new_user
+    channel_id = channels_create(user_A['token'], "Private_channel", False) # Create a new private channel
     
     '''Create another user which don't have authority to this channel'''
     user_B = auth_register('labalab.love@gmail.com', 'qweASD5200a01', 'Yellow', 'Re')
@@ -37,16 +37,29 @@ def test_channel_join_user_not_auth(new_user):
     
 # user successfully leave the channel
 def test_channel_leave_success(new_user):
-    u_id, token = new_user['u_id'], new_user['token']
-    private_channal_id = channels_create(token, 'private_channel') #Create private channel
-    public_channel_id = channels_create(token, 'public_channel')   #Create public channel
-    channel_leave(token, public_channel_id)
-    channel_leave(token, private_channal_id)
+    user = new_user
+    private_channal_id = channels_create(user['token'], 'private_channel') #Create private channel
+    public_channel_id = channels_create(user['token'], 'public_channel')   #Create public channel
+    channel_leave(user['token'], public_channel_id)
+    channel_leave(user['token'], private_channal_id)
     
 def test_channel_leave_inexist_user(new_user):
-    pass
+    user_A = new_user
+    public_channel_id = channels_create(user['token'], 'public_channel', True)      # User A create a public channel and
+    private_channel_id = channels_create(user['token'], 'private_channel', False)   # a private channel
+    
+    '''Create another user which is not in this two channel'''
+    user_B = auth_register('labalab.love@gmail.com', 'qweASD5200a01', 'Yellow', 'Re')
+    auth_login('labalab.love@gmail.com','qweASD5200a01')
+    with pytest.raises(AccessError):
+        channel_leave(user_B['token'], public_channel_id)
+        channel_leave(user_B['token'], private_channel_id)
 
 
 
-def test_channel_leave_channel_id_invalid():
-    pass
+
+def test_channel_leave_channel_id_invalid(new_user):
+    user = new_user
+    channel_id = channels_create(user['token'], 'channel_A', true)
+    with pytest.raises(InputError):
+        channel_leave(user['token'], channel_id + 1)
