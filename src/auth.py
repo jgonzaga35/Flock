050a5be3f33c0967dc4aff16af889daddf738a9a
Peter <lplsz2000@gmail.com>
2020-09-26 20:48:01 +1000

    Fix not detecting for used email when register

--- src/auth.py
+++ src/auth.py
@@ -34,6 +34,10 @@ def auth_logout(token):
 def auth_register(email, password, name_first, name_last):
     input_error_checking(email, password, name_first, name_last)
 
+    for user in database['users']:
+        if user['email'] == email:
+            raise InputError("Email has been used.")
+
     u_id = highest_id() + 1
     new_user = {
         'email': email,
