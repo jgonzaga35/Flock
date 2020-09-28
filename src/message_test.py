225f75b59c6a16e7bd5c4c2c09114de312a01f1a
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-09-28 18:04:06 +1000

    message_send/test: send multiple messages from the owner

--- src/message_test.py
+++ src/message_test.py
@@ -34,5 +34,30 @@ def test_message_send_not_member():
     with pytest.raises(AccessError):
         message_send(userb['token'], channel_id, 'first message')
 
+def test_message_send_multiple_messages():
+    clear_database()
 
-# TODO: test send message with user join
+    usera = auth_register("email@a.com", "averylongpassword", "A", "LastA")
+    channel_id = channels_create(usera['token'], "channela", is_public=True)['channel_id']
+
+    time_before_message_send = time.time()
+
+    message_send(usera['token'], channel_id, "first message")
+    message_send(usera['token'], channel_id, "second message")
+    message_send(usera['token'], channel_id, "third message")
+
+    messages = channel_messages(usera['token'], channel_id, start=0)['messages']
+
+    assert len(messages) == 3
+
+    assert messages[0]['message'] == "third message"
+    assert messages[1]['message'] == 'second message'
+    assert messages[2]['message'] == 'first message'
+
+    for message in messages:
+        assert message['u_id'] == usera['id']
+        assert message['time_created'] >= time_before_message_send
+
+
+# TODO: test_message_send_member_but_not_owner (need to have channel join, not yet on this branch)
+# TODO: test_message_send_multiple_members (need channel_join) 
