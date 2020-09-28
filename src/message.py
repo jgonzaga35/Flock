3cb39f9a784114601ffddcea32ac4ef9ff8671be
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-09-28 18:22:24 +1000

    message_send: write implementation

--- src/message.py
+++ src/message.py
@@ -1,6 +1,43 @@
+import time
+from database import database
+from auth import auth_get_current_user_id_from_token
+from error import AccessError, InputError
+
+
 def message_send(token, channel_id, message):
+    if token not in database["active_tokens"]:
+        raise AccessError("not logged in")
+
+    user_id = auth_get_current_user_id_from_token(token)
+
+    channel = None
+    for ch in database["channels"]:
+        if ch["id"] == channel_id:
+            channel = ch
+            break
+
+    if channel is None:
+        raise InputError(f"invalid channel id {channel_id}")
+
+    if user_id not in channel["all_members_id"]:
+        raise AccessError(
+            f"user {user_id} needs to be a member of this channel to send a message"
+        )
+
+    channel["message_id_head"] += 1
+
+    new_message = {
+        "message_id": channel["message_id_head"],
+        "u_id": user_id,
+        "message": message,
+        "time_created": time.time(),
+    }
+
+    # we store the newest messages first
+    channel["messages"].insert(0, new_message)
+
     return {
-        "message_id": 1,
+        # broken!
     }
 
 
--- src/message_test.py
+++ src/message_test.py
@@ -7,62 +7,71 @@ from channel import channel_messages
 from message import message_send
 from error import AccessError
 
+
 def test_send_one_message():
     clear_database()
 
     usera = auth_register("email@a.com", "averylongpassword", "A", "LastA")
-    channel_id = channels_create(usera['token'], "channela", is_public=True)['channel_id']
+    channel_id = channels_create(usera["token"], "channela", is_public=True)[
+        "channel_id"
+    ]
 
     time_before_message_send = time.time()
 
-    message_details = message_send(usera['token'], channel_id, 'first message')
-    assert 'message_id' in message_details
-    assert isinstance(message_details['message_id'], int)
+    message_details = message_send(usera["token"], channel_id, "first message")
+    assert "message_id" in message_details
+    assert isinstance(message_details["message_id"], int)
 
-    messages = channel_messages(usera['token'], channel_id, start=0)['messages']
-    assert messages[0]['u_id'] == usera['token']
-    assert messages[0]['message'] == 'first message'
-    assert messages[0]['time_created'] >= time_before_message_send
+    messages = channel_messages(usera["token"], channel_id, start=0)["messages"]
+    assert messages[0]["u_id"] == usera["u_id"]
+    assert messages[0]["message"] == "first message"
+    assert messages[0]["time_created"] >= time_before_message_send
+
 
 def test_message_send_not_member():
     clear_database()
 
     usera = auth_register("email@a.com", "averylongpassword", "A", "LastA")
     userb = auth_register("email@b.com", "averylongpassword", "b", "LastB")
-    channel_id = channels_create(usera['token'], "channela", is_public=True)['channel_id']
+    channel_id = channels_create(usera["token"], "channela", is_public=True)[
+        "channel_id"
+    ]
 
     with pytest.raises(AccessError):
-        message_send(userb['token'], channel_id, 'first message')
+        message_send(userb["token"], channel_id, "first message")
+
 
 def test_message_send_multiple_messages():
     clear_database()
 
     usera = auth_register("email@a.com", "averylongpassword", "A", "LastA")
-    channel_id = channels_create(usera['token'], "channela", is_public=True)['channel_id']
+    channel_id = channels_create(usera["token"], "channela", is_public=True)[
+        "channel_id"
+    ]
 
     time_before_message_send = time.time()
 
-    message_send(usera['token'], channel_id, "first message")
-    message_send(usera['token'], channel_id, "second message")
-    message_send(usera['token'], channel_id, "third message")
+    message_send(usera["token"], channel_id, "first message")
+    message_send(usera["token"], channel_id, "second message")
+    message_send(usera["token"], channel_id, "third message")
 
-    messages = channel_messages(usera['token'], channel_id, start=0)['messages']
+    messages = channel_messages(usera["token"], channel_id, start=0)["messages"]
 
     assert len(messages) == 3
 
-    assert messages[0]['message'] == "third message"
-    assert messages[1]['message'] == 'second message'
-    assert messages[2]['message'] == 'first message'
+    assert messages[0]["message"] == "third message"
+    assert messages[1]["message"] == "second message"
+    assert messages[2]["message"] == "first message"
 
     message_ids = []
     for message in messages:
-        assert message['u_id'] == usera['id']
-        assert message['time_created'] >= time_before_message_send
+        assert message["u_id"] == usera["u_id"]
+        assert message["time_created"] >= time_before_message_send
 
         # make sure all the ids are unique
-        assert message['message_id'] not in message_ids
-        message_ids.append(message['message_id'])
+        assert message["message_id"] not in message_ids
+        message_ids.append(message["message_id"])
 
 
 # TODO: test_message_send_member_but_not_owner (need to have channel join, not yet on this branch)
-# TODO: test_message_send_multiple_members (need channel_join) 
+# TODO: test_message_send_multiple_members (need channel_join)
