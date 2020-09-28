a6b3ea54a0513c8c4861a0dc6f616711635794da
Eric Bai <baixu.must@gmail.com>
2020-09-28 17:03:21 +1000

    add tests for channel_add_owner

--- src/channel_test.py
+++ src/channel_test.py
@@ -7,11 +7,23 @@ import pytest
 
 
 # register a new user and log in, return the dictionary including u_id and token
+def register_one_user():
+    """ Registers sample users """
+    paira = auth_register("email@a.com", "averylongpassword", "A", "LastA")
+    return paira
+
 def register_a_and_b():
     """ Registers sample users """
     paira = auth_register("email@a.com", "averylongpassword", "A", "LastA")
     pairb = auth_register("email@b.com", "averylongpassword", "B", "LastB")
     return paira, pairb
+
+def register_a_b_and_c():
+    """ Registers sample users """
+    paira = auth_register("email@a.com", "averylongpassword", "A", "LastA")
+    pairb = auth_register("email@b.com", "averylongpassword", "B", "LastB")
+    pairc = auth_register("email@c.com", "averylongpassword", "C", "LastC")
+    return paira, pairb, pairc
 
 # Join the channel successfully
 def test_join_channel_successfully():
@@ -131,6 +143,49 @@ def test_channel_details_invalid_id():
     # fixme: this should be done with channel create
     with pytest.raises(InputError):
         channel_details(usera['token'], 1)
+
+def test_add_owner_successfully():
+    clear_database()
+    user_A, user_B = register_a_and_b()
+    private_channel = channels_create(user_A['token'], "private_channel", False)
+    channel_addowner(user_A['token'], private_channel['channel_id'], user_B['u_id'])
+
+def test_add_owner_with_invalid_channel_id():
+    clear_database()
+    user_A, user_B = register_a_and_b()
+    public_channel = channels_create(user_A['token'], "public_channel", True)
+    channel_join(user_A['token'], public_channel)
+    with pytest.raises(InputError):
+        channel_addowner(user_A['token'], private_channel['channel_id'] + 1, user_B['u_id']) #channel_id + 1 is invalid
+
+def test_add_owner_repeatedly():
+    clear_database()
+    user_A, user_B = register_a_and_b()
+    private_channel = channels_create(user_A['token'], "private_channel", False)
+    with pytest.raises(InputError):
+        channel_addowner(user_A['token'], private_channel['channel_id'], user_A['u_id'])
+
+def test_add_invalid_user_to_owner():
+    clear_database()
+    user_A = register_one_user()
+    private_channel = channels_create(user_A['token'], "private_channel", False)
+    invalid_uid = 233
+    with pytest.raises(InputError):
+        channel_addowner(user_A['token'], private_channel['channel_id'], invalid_uid)
+
+
+# We assume all of the user are in a public channel
+# One of them are the owner whereas other two are common user in this channel
+def test_add_owner_by_non_owner():
+    clear_database()
+    user_A, user_B, user_C = register_a_b_and_c()
+    public_channel = channels_create(user_A['token'], "public_channel", True) #user_A create the channel and be the owner
+    channel_join(user_C['token'], public_channel['channel_id']) # Other two user join in the channel
+    channel_join(user_B['token'], public_channel['channel_id'])
+    with pytest.raises(AccessError):
+        channel_addowner(user_B['token'], public_channel['channel_id'], user_C['u_id']) # user_B who are not owner add common user
+                                                                                        # user_C be the owner
+
 
 if __name__ == '__main__':
     test_join_channel_without_authority(fixture_new_user)
