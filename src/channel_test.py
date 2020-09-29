c56e620716e7c83021efb43dbeba04322b425d40
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-09-29 11:13:50 +1000

    function name change

--- src/channel_test.py
+++ src/channel_test.py
@@ -12,7 +12,7 @@ import pytest
 #################################################################################
 def test_messages_no_messages():
     clear_database()
-    user = register_and_login_user(
+    user = register_user(
         'validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith'
     )
     channel = channels_create(user['token'], 'channel', is_public = True)
@@ -24,7 +24,7 @@ def test_messages_no_messages():
     
 def test_messages_invalid_channel_ID():
     clear_database()
-    user = register_and_login_user(
+    user = register_user(
         'validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith'
     )
     invalid_channel_id = -1
@@ -33,10 +33,10 @@ def test_messages_invalid_channel_ID():
 
 def test_messages_user_not_member():
     clear_database()
-    user_01= register_and_login_user(
+    user_01= register_user(
         'validemail01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith'
     )
-    user_02 = register_and_login_user(
+    user_02 = register_user(
         'validemail02@gmail.com', 'validpass@!owner02', 'John', 'Smith'
     )
     channel = channels_create(user_01['token'], 'channel_01', is_public = True)
@@ -49,7 +49,7 @@ def test_messages_user_not_member():
 # def test_messages_negative_start_index():
 #     clear_database()
     # # Add a user and log them in
-    # user = register_and_login_user('validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
+    # user = register_user('validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
     
     # # Create a channel and fill with messages
     # channel = channels_create(user['token'], 'new_channel', is_public = True)
@@ -60,7 +60,7 @@ def test_messages_user_not_member():
 # def test_messages_simple():
 #     clear_database()
 #     # Add a user and log them in
-#     user = register_and_login_user('validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
+#     user = register_user('validemailowner01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
     
 #     # Create a channel and fill with messages
 #     channel = channels_create(user['token'], 'new_channel', is_public = True)
@@ -70,7 +70,7 @@ def test_messages_user_not_member():
 
 # def test_messages_start_overflow():
     # clear_database()
-    # user = register_and_login_user('validemail01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
+    # user = register_user('validemail01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
     # channel = channels_create(user['token'], 'channel_01', is_public = True)
     # channel_join(user['token'], channel['channel_id'])
     # message_send(user['token'], channel['channel_id'], 'Hello World!')
@@ -79,7 +79,7 @@ def test_messages_user_not_member():
 
 # def test_messages_start_underflow():
 #     clear_database()
-#     user = register_and_login_user('validemail01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
+#     user = register_user('validemail01@gmail.com', 'validpass@!owner01', 'Bob', 'Smith')
 #     channel = channels_create(user['token'], 'channel_01', is_public = True)
 #     channel_join(user['token'], channel['channel_id'])
 #     message_send(user['token'], channel['channel_id'], 'Hello World!')
@@ -94,20 +94,20 @@ def test_messages_user_not_member():
 
 # ---------------------------------------------------------------------------------------------
 # Returns {u_id, token}
-def register_and_login_user(email, password, name_first, name_last):
+def register_user(email, password, name_first, name_last):
     user = auth_register(email, password, name_first, name_last)
     return user 
 
 # Helper function that creates a sample channel with 3 users (including 1 owner)
 def create_sample_channel():
     # Register a owner and two users and logs them in
-    owner_credentials = register_and_login_user(
+    owner_credentials = register_user(
         'validemailowner@gmail.com', 'validpass@!owner', 'Channel', 'Owner'
     )
-    user_01_credentials = register_and_login_user(
+    user_01_credentials = register_user(
         'validemail01@gmail.com', 'validpass@!01', 'First', 'User'
     )
-    user_02_credentials = register_and_login_user(
+    user_02_credentials = register_user(
         'validemail@gmail.com', 'validpass@!02', 'Second', 'User'
     )
     
