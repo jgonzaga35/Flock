7b7afbf826b4899dfcc6974024b4310c5c0e12ad
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-10-09 16:35:12 +1100

    move url test helper to test_helpers

--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,4 +1,9 @@
+import pytest
+import re
+import signal
 from auth import auth_register
+from subprocess import Popen, PIPE
+from time import sleep
 
 def register_n_users(num_users):
     """
@@ -23,3 +28,25 @@ def register_n_users(num_users):
     assert len(users) == num_users
 
     return users
+
+# Use this fixture to get the URL of the server. It starts the server for you,
+# so you don't need to.
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
