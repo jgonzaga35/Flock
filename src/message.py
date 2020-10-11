43b957d5e3024eadd5bcf3bb908f98eee8626a43
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-11 19:42:02 +1100

    added doc string and comments

--- src/message.py
+++ src/message.py
@@ -8,5 +8,12 @@ def message_remove(token, message_id):
     }
 
 def message_edit(token, message_id, message):
+    '''
+    Given a message, update it's text with new text. 
+    If the new message is an empty string, the message is deleted.
+    '''
+    # Check user is valid
+    # Check message_id is valid
+    # Check user is authorised to edit message
     return {
     }
