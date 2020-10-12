9f51c23bc5a77583b740b4d03462c9df1915a384
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-12 23:11:18 +1100

    added extra tests

--- src/message_test.py
+++ src/message_test.py
@@ -1,8 +1,4 @@
-from message import message_send, message_edit
 from database import clear_database
-from auth import auth_register
-from channels import channels_create
-
 from test_helpers import url, register_n_users
 
 from error import AccessError
@@ -23,7 +19,6 @@ def test_edit_non_existenet_user(url):
         'is_public': True,
     }
     channel = requests.post(url + 'channels/create', json=channel_params)
-    
     # User sends a message
     message_params = {
         'token': user['token'],
@@ -42,7 +37,7 @@ def test_edit_non_existenet_user(url):
     
     assert r.status_code == 403
     
-def test_edit_invalid_message_id():
+def test_edit_invalid_message_id(url):
     user = register_n_users(1)
     # Create a new channel
     channel_params = {
@@ -71,7 +66,7 @@ def test_edit_invalid_message_id():
     assert r.status_code == 403
 
 # User editing a message is not authorised to edit it
-def test_edit_unauthorised_user():
+def test_edit_unauthorised_user(url):
     user01, user02 = register_n_users(2)
     
     # Create a new channel
@@ -101,7 +96,7 @@ def test_edit_unauthorised_user():
     assert r.status_code == 403
 
 # Test that owner of channel can edit any message
-def test_edit_owner():
+def test_edit_owner(url):
     user01, user02 = register_n_users(2)
     
     # Create a new channel with User01 as admin
@@ -110,7 +105,7 @@ def test_edit_owner():
         'name': 'channel_01',
         'is_public': True,
     }
-    channel = requests.pst(url + 'channels/create', json=channel_params)
+    channel = requests.post(url + 'channels/create', json=channel_params)
     
     # User 2 sends a message
     message_params = {
@@ -134,7 +129,44 @@ def test_edit_owner():
         'channel_id': channel['channel_id'],
         'start': 0,
     }
+    channel_messages = requests.get(url + 'channel/messages', json=channel_messages_params)
+    # TODO How to make this blackbox? This adheres to a dict database (currently list)
+    assert message['message_id']['message'] == channel_messages['message_id']['message']
     
-    assert channel_messages_params['messages'][0] == 'try editing'
+def test_edit_empty_string(url):
+    # Tests that a message is deleted is edit is empty string
+    user = register_n_users(1)
     
+    # Create a new channel
+    channel_params = {
+        'token': user['token'],
+        'name': 'channel_01',
+        'is_public': True,
+    }
+    channel = requests.post(url + 'channels/create', json=channel_params)
     
+    # Send message
+    message_params = {
+        'token': user02['token'],
+        'channel_id': channel['channel_id'],
+        'message': 'test message',
+    }
+    message = requests.post(url + 'message/send', json=message_params)
+    
+    # Edit message with empty string
+    message_edit = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'message': '',
+    }
+    requests.put(url + 'message/edit', json=message_edit)
+    
+    # Ensure message id not in database
+    channel_messages_params = {
+        'token': user['token'],
+        'channel_id': channel['channel_id'],
+        'start': 0,
+    }
+    channel_messages = requests.get(url + 'channel/messages', json=channel_messages_params)
+     # TODO This adheres to a dict database (currently list)
+    assert message['message_id'] not in channel_messages['messages']
