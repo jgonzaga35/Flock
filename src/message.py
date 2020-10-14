e1942b0f2be24b2e173c0927573a3b61d9ec5462
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-15 00:12:52 +1100

    added docstring

--- src/message.py
+++ src/message.py
@@ -1,3 +1,5 @@
+from database import database
+
 def message_send(token, channel_id, message):
     return {
         "message_id": 1,
@@ -5,6 +7,9 @@ def message_send(token, channel_id, message):
 
 
 def message_remove(token, message_id):
+    '''
+        Given a message_id for a message, this message is removed from the channel
+    '''
     return {}
 
 
