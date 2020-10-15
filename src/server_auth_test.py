166bfca2ef6eca4eed8bdee746f163f84660fcc0
z5314408 <lplsz2000@gmail.com>
2020-10-15 18:35:44 +1100

    Add some more test

--- src/server_auth_test.py
+++ src/server_auth_test.py
@@ -18,7 +18,7 @@ def register_new_account(url):
         }
     ).json()
 
-
+# Test for auth_login
 def test_login_success_case(url):
     requests.delete(url + "clear")
     user1 = register_new_account(url)
@@ -68,3 +68,98 @@ def test_login_never_registered(url):
     requests.delete(url + "clear")
     r = requests.post(url + "auth/login", json={"email": "didntusethis@gmail.com", "password": "123abcd!@#"})
     assert r.status_code == 400
+
+# Test for auth_logout
+def test_logout_fail_twice(url):
+    requests.delete(url + "clear")
+    user1 = register_new_account(url)
+    requests.post(url + "auth/logout", json={"token": user1["token"]})
+    assert requests.post(url + "auth/logout", json={"token": user1["token"]}).json()["is_success"] == False
+
+def test_logout_fail_non_exist_user():
+    requests.delete(url + "clear")
+    token = "non exist"
+    assert requests.post(url + "auth/logout", json={"token": token}).json()["is_success"] == False
+
+
+def test_logout_success():
+    requests.delete(url + "clear")
+    token = register_new_account(url)["token"]
+    assert requests.post(url + "auth/logout", json={"token": token}).json()["is_success"] == True
+
+# Test for auth_register
+def test_auth_register_invalid_email():
+    requests.delete(url + "clear")
+    r1 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "didntusethis@gmail",
+            "password": "123abc!@#",
+            "name_first": "Hayden",
+            "name_last": "Everest"
+        }
+    )
+    r2 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "didntusethis.com",
+            "password": "123abc!@#",
+            "name_first": "Hayden",
+            "name_last": "Everest"
+        }
+    )
+    assert r1.status_code == 400
+    assert r2.status_code == 400
+
+
+def test_auth_register_used_email():
+    requests.delete(url + "clear")
+    register_new_account(url)
+    r = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail@gmail.com",
+            "password": "123abc!@#",
+            "name_first": "Hayden",
+            "name_last": "Everest"
+        }
+    )
+    assert r.status_code == 400
+
+
+def test_auth_register_weak_password():
+    requests.delete(url + "clear")
+    r = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail@gmail.com",
+            "password": "LOL",
+            "name_first": "Hayden",
+            "name_last": "Everest"
+        }
+    )
+    assert r.status_code == 400
+
+
+def test_auth_register_wrong_name():
+    requests.delete(url + "clear")
+    r1 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail@gmail.com",
+            "password": "123abc!@#",
+            "name_first": "dsjfsdkfjsdafklsdjfsdklfjlkasdkflasdjkfjklsdafjklasdkjlflksjadfjklsdakjfjkdsaflkjadslkflkasdklfklkljdsafl",
+            "name_last": "Everest"
+        }
+    )
+    r2 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail@gmail.com",
+            "password": "123abc!@#",
+            "name_first": "Hayden",
+            "name_last": "asdfjskaldjflsadfjklasdfjaksldfjakjsdhfsjkadhfkjasdhfkjsdhfkjasdfhkjsadhfkjasdhf"
+        }
+    )
+    assert r1.status_code == 400
+    assert r1.status_code == 400
