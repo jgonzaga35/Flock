1aa8013ae2e723c518c1b29cf90147036003fa35
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-15 23:34:15 +1100

    added tests for message_edit

--- src/message_test.py
+++ src/message_test.py
@@ -1,12 +1,16 @@
+from message import message_send, message_remove, message_edit
+from auth import auth_register
+from channel import channel_messages, channel_join
+from channels import channels_create, channels_list
+from test_helpers import register_n_users
+from database import database, clear_database
+
 import time
 import pytest
-from auth import auth_register
-from database import clear_database
-from channels import channels_create
-from channel import channel_messages
-from message import message_send, message_remove, message_edit
 from error import AccessError, InputError
 
+INVALID_USER_TOKEN = -1
+INVALID_MESSAGE_ID = -1
 
 def test_send_one_message():
     clear_database()
@@ -117,6 +121,62 @@ def test_message_remove():
     # just to make coverage happy
     message_remove(-1, -1)
 
+##################################################################################
+#                           Tests for message_edit                               #
+##################################################################################
+def test_remove_invalid_user_token():
+    clear_database()
+    user = register_n_users(1)
 
-def test_message_edit():
-    message_edit(-1, -1, -1)
+    # Create a new channel
+    channel = channels_create(user["token"], "channel01", is_public=True)
+    # User sends a message
+    message = message_send(user["token"], channel["channel_id"], "test message")
+    # Non-existent user tries to edit the message
+    with pytest.raises(AccessError):
+        assert message_edit(INVALID_USER_TOKEN, message["message_id"], 'edited message')
+        
+def test_remove_invalid_message_id():
+    clear_database()
+    user = register_n_users(1)
+
+    # Create a new channel
+    channel = channels_create(user["token"], "channel01", is_public=True)
+    # User sends a message
+    message_send(user["token"], channel["channel_id"], "test message")
+    # User tries to edit message with an invalid message id (doesn't exist)
+    with pytest.raises(InputError):
+        assert message_edit(user["token"], INVALID_MESSAGE_ID, 'edited message')
+
+# User tries to edit a message that they are not authorised to edit
+def test_remove_unauthorised_user():
+    clear_database()
+    user01, user02 = register_n_users(2)
+
+    # Create channel with message from user01
+    channel = channels_create(user01["token"], "channel", is_public=True)
+    channel_join(user02["token"], channel["channel_id"])
+    message = message_send(user01["token"], channel["channel_id"], "test message")
+    # User02 tries to remove message from user01
+    with pytest.raises(AccessError):
+        message_edit(user02["token"], message["message_id"], 'edited message')
+
+# Test that the owner of the flockr can edit any message
+def test_remove_owner_flock_permissions():
+    clear_database()
+    user01, user02 = register_n_users(2)
+
+    # Create a new channel with user01 as admin
+    channel = channels_create(user01["token"], "channel", is_public=True)
+    channel_join(user02["token"], channel["channel_id"])
+
+    message = message_send(user02["token"], channel["channel_id"], "test message")
+    message_edit(user01["token"], message["message_id"], 'edited message')
+    
+    # Message stil exists and is edited
+    assert message["message_id"] in [
+        x["message_id"]
+        for x in database["channels"][channel["channel_id"]]["messages"].values()
+    ]
+    
+    assert database["channels"][channel["channel_id"]]["messages"][message['message_id']]['message'] == 'edited message'
