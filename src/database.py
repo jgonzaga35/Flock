dbebed822a7bf406b978a71193634fc1fca703e9
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-15 16:54:08 +1100

    minor change

--- src/database.py
+++ src/database.py
@@ -4,23 +4,23 @@ database = {
     # id_heads are only ever incremented, they are used to generate ids
     "channels_id_head": 1,
     "channels": {
-        # 1: {
-        #     "id": 1,
-        #     "name": "greatest_channel",
-        #     # the user id of the owners
-        #     "owner_members_id": [1, 2, 3]
-        #     # the user id of all the members (including the owners)
-        #     "all_members_id": [1, 2, 3, 5, 4, 9]
-        #     "is_public": True
-        #     "messages": {
-        #           1: {
-        #               "message_id": 1,
-        #               "u_id": 1,
-        #               "message": "Hello world",
-        #               "time_created": 1582426789,
-        #           }
-        #       },
-        # },
+        73: {
+            "id": 73,
+            "name": "greatest_channel",
+            # the user id of the owners
+            "owner_members_id": [101, 102],
+            # the user id of all the members (including the owners)
+            "all_members_id": [101, 102],
+            "is_public": True,
+            "messages": {
+                  1: {
+                      "message_id": 1,
+                      "u_id": 1,
+                      "message": "Hello world",
+                      "time_created": 1582426789,
+                  }
+              },
+        },
     },
     # no message should have the same id, even across channels
     "message_id_head": 1,
