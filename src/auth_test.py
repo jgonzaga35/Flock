c8584e641435dcb30800e4a8dbb0f91db1d0749a
Peter <lplsz2000@gmail.com>
2020-10-15 11:02:52 +1100

    Delete a redundant test

--- src/auth_test.py
+++ src/auth_test.py
@@ -119,8 +119,3 @@ def test_auth_helper_user_data_from_invalid_id():
     clear_database()
     with pytest.raises(KeyError):
         auth_get_user_data_from_id(user_id=-1)
-
-def test_encrypt():
-    test_password = ["abc123456", "LOLGGNOOB", "123abc!@#"]
-    for password in test_password:
-        assert encrypt(password) == sha256(password.encode()).hexdigest()
