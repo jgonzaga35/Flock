f57c5d386ae94fe09686cffa09943af49e488f69
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-19 19:44:28 +1100

    fixed tests for remove and added implementation

--- src/server.py
+++ src/server.py
@@ -4,8 +4,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from error import InputError
 from auth import auth_login, auth_logout, auth_register
-from message import message_remove
+from message import message_remove, message_send
 from other import clear
+from channel import channel_join, channel_messages
+from channels import channels_create
 
 
 def defaultHandler(err):
@@ -40,6 +42,7 @@ def echo():
 @APP.route("/clear", methods=["DELETE"])
 def delete():
     clear()
+    return({})
 
 # Auth_functions
 @APP.route("/auth/login", methods=["POST"])
@@ -66,21 +69,46 @@ def register():
 #############################################################################
 #                           Routes for messages                             #
 #############################################################################
+# @APP.route("/channels/create", methods=["POST"])
+# def channels_create_handler():
+#     data = request.get_json()
 
-
-@APP.route("/channels/create", methods=["POST"])
-def channels_create_handler():
-    data = request.get_json()
-
-    token = data["token"]
-    name = data["name"]
-    is_public = data["is_public"]
+#     token = data["token"]
+#     name = data["name"]
+#     is_public = data["is_public"]
     
-    return jsonify(channels_create(token, name, is_public))
+#     return jsonify(channels_create(token, name, is_public))
 
+# @APP.route("/message/send", methods=["POST"])
+# def send_message():
+#     data = request.get_json()
+
+#     token = data["token"]
+#     channel_id = data["channel_id"]
+#     message = data["message"]
+
+#     return jsonify(message_send(token, channel_id, message))
+
+# @APP.route("/channel/join", methods=["POST"])
+# def join_channel():
+#     data = request.get_json()
+
+#     token = data["token"]
+#     channel_id = data["channel_id"]
+#     return dumps({channel_join(token, channel_id)})
+
+# @APP.route("/channel/messages", methods=["GET"])
+# def messages_channel():
+#     token = request.args.get("token")
+#     channel_id = request.args.get("channel_id")
+#     start = request.args.get("start")
+
+#     return dumps({
+#         channel_messages(token, channel_id, start)
+#     })
 
 @APP.route("/message/remove", methods=["DELETE"])
-def message_remove():
+def remove_message():
     data = request.get_json()
     return dumps(
         message_remove(data["token"], data["message_id"])
--- src/server_message_test.py
+++ src/server_message_test.py
@@ -23,130 +23,156 @@ def register_new_account(url):
 def test_remove_invalid_user_token(url):
     requests.delete(url + "clear")
     user = register_new_account(url)
-    print(user)
     # Create a new channel
     channel_params = {
         "token": user["token"],
         "name": "channel_01",
         "is_public": True,
     }
-    response = requests.post(url + "channels/create", json=channel_params)
-    print(response.status_code)
-    channel = response.json()
-    print(channel)
+    channel = requests.post(url + "channels/create", json=channel_params).json()
     # User sends a message
     message_params = {
         "token": user["token"],
         "channel_id": channel["channel_id"],
         "message": "test message",
     }
-    message = reqeusts.post(url +  "message/send", json=message_params).json()
-    
-    # Non-existent user tries to remove the message
-    # TODO Ensure token implementation is correct
+    message = requests.post(url +  "message/send", json=message_params).json()
     message_remove_params = {
         "token": user["token"] + 1,
         "message_id": message["message_id"],
     }
     
     # Ensure access error raised
-    r = reqeusts.delete(url + "message/remove", json=message_remove_params)
+    r = requests.delete(url + "message/remove", json=message_remove_params)
     assert r.status_code == 403
 
-# def test_remove_invald_message_id(url):
-#     requests.delete(url + "clear")
-#     user = register_new_account()
-#      # Create a new channel
-#     channel_params = {
-#         "token": user["token"],
-#         "name": "channel_01",
-#         "is_public": True,
-#     }
-#     channel = requests.post(url + "/channels/create", json=channel_params).json()
+def test_remove_invald_message_id(url):
+    requests.delete(url + "clear")
+    user = register_new_account(url)
+     # Create a new channel
+    channel_params = {
+        "token": user["token"],
+        "name": "channel_01",
+        "is_public": True,
+    }
+    channel = requests.post(url + "/channels/create", json=channel_params).json()
     
-#     # User sends a message
-#     message_params = {
-#         "token": user["token"],
-#         "channel_id": channel["channel_id"],
-#         "message": "test message",
-#     }
-#     message = requests.post(url +  "/message/send", json=message_params).json()
+    # User sends a message
+    message_params = {
+        "token": user["token"],
+        "channel_id": channel["channel_id"],
+        "message": "test message",
+    }
+    message = requests.post(url +  "/message/send", json=message_params).json()
     
-#     # User tries to edit message with invalid message id
-#     message_remove_params = {
-#         "token": user["token"],
-#         "message_id": INVALID_MESSAGE_ID
-#     }
+    # User tries to edit message with invalid message id
+    message_remove_params = {
+        "token": user["token"],
+        "message_id": INVALID_MESSAGE_ID
+    }
     
-#     # Ensure input error raised
-#     r = requests.delete(url + "message/remove", json=message_remove_params)
-#     assert r.status_code == 422
+    # Ensure input error raised
+    r = requests.delete(url + "message/remove", json=message_remove_params)
+    assert r.status_code == 400
 
 
-# # User removing a message is not authorised to remove it
-# def test_remove_unauthorised_user(url):
-#     requests.delete(url + "clear")
-#     user01, user02 = register_n_users(2)
+# User removing a message is not authorised to remove it
+def test_remove_unauthorised_user(url):
+    requests.delete(url + "clear")
+    user01 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail01@gmail.com",
+            "password": "123abc01!@#",
+            "name_first": "Hayden01",
+            "name_last": "Everest01",
+        },
+    ).json()
+    user02 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail02@gmail.com",
+            "password": "123abc02!@#",
+            "name_first": "Hayden02",
+            "name_last": "Everest02",
+        },
+    ).json()
     
-#     # Create a new channel
-#     channel_params = {
-#         "token": user01["token"],
-#         "name": "channel_01",
-#         "is_public": True,
-#     }
-#     channel = requests.post(url + "channels/create", json=channel_params).json()
+    # Create a new channel
+    channel_params = {
+        "token": user01["token"],
+        "name": "channel_01",
+        "is_public": True,
+    }
+    channel = requests.post(url + "channels/create", json=channel_params).json()
 
-#     # User 1 sends a message
-#     message_params = {
-#         "token": user01["token"],
-#         "channel_id": channel["channel_id"],
-#         "message": "test message",
-#     }
-#     message = requests.post(url + "message/send", json=message_params).json()
+    # User 1 sends a message
+    message_params = {
+        "token": user01["token"],
+        "channel_id": channel["channel_id"],
+        "message": "test message",
+    }
+    message = requests.post(url + "message/send", json=message_params).json()
 
-#     # User 2 tries to remove message from User 1
-#     message_remove_params = {
-#         "token": user02["token"],
-#         "message_id": message["message_id"],
-#     }
+    # User 2 tries to remove message from User 1
+    message_remove_params = {
+        "token": user02["token"],
+        "message_id": message["message_id"],
+    }
     
-#     r = requests.delete(url + "message/remove", json=message_remove_params)
-#     assert r.status_code == 403
+    r = requests.delete(url + "message/remove", json=message_remove_params)
+    assert r.status_code == 403
 
-# # Test that owner of channel can remove any message
-# def test_remove_owner(url):
-#     requests.delete(url + "clear")
-#     user01, user02 = register_n_users(2)
+# Test that owner of channel can remove any message
+def test_remove_owner(url):
+    requests.delete(url + "clear")
+    user01 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail01@gmail.com",
+            "password": "123abc01!@#",
+            "name_first": "Hayden01",
+            "name_last": "Everest01",
+        },
+    ).json()
+    user02 = requests.post(
+        url + "/auth/register",
+        json={
+            "email": "validemail02@gmail.com",
+            "password": "123abc02!@#",
+            "name_first": "Hayden02",
+            "name_last": "Everest02",
+        },
+    ).json()
     
-#     # Create a new channel with User01 as admin
-#     channel_params = {
-#         "token": user01["token"],
-#         "name": "channel_01",
-#         "is_public": True,
-#     }
-#     channel = requests.post(url + "channels/create", json=channel_params).json()
-
-#     # User 2 sends a message
-#     message_params = {
-#         "token": user02["token"],
-#         "channel_id": channel["channel_id"],
-#         "message": "test message",
-#     }
-#     message = requests.post(url + "message/send", json=message_params).json()
-
-#     # User01 (admin) removes it
-#     message_remove_params = {
-#         "token": user01["token"],
-#         "message_id": message["message_id"],
-#     }
-#     requests.delete(url + "message/remove", json=message_remove_params)
+    # Create a new channel with User01 as admin
+    channel_params = {
+        "token": user01["token"],
+        "name": "channel_01",
+        "is_public": True,
+    }
+    channel = requests.post(url + "channels/create", json=channel_params).json()
     
-#     # Confirm that message was removed successfuly
-#     channel_message_params = {
-#         "token": user01["token"],
-#         "channel_id": channel["channel_id"],
-#         "start": 0,
-#     }
-#     channel_messages = requests.get(url + "channel/messages", json=channel_message_params)
-#     # TODO Ensure this is correct
-#     assert message["message_id"] not in channel_messages
+    # User 2 sends a message
+    requests.post(url + "channel/join", json={"token": user02["token"], "channel_id": channel["channel_id"]})
+    message_params = {
+        "token": user02["token"],
+        "channel_id": channel["channel_id"],
+        "message": "test message",
+    }
+    message = requests.post(url + "message/send", json=message_params).json()
+    # User01 (admin) removes it
+    message_remove_params = {
+        "token": user01["token"],
+        "message_id": message["message_id"],
+    }
+    requests.delete(url + "message/remove", json=message_remove_params)
+    
+    # Confirm that message was removed successfuly
+    channel_message_params = {
+        "token": user01["token"],
+        "channel_id": channel["channel_id"],
+        "start": 0,
+    }
+    channel_messages = requests.get(url + "channel/messages", params=channel_message_params)
+    assert channel_messages.status_code == 403
