7bc4835cd6298d922a23bd79efd1762a5bd6257a
Justin Gonzaga <justingonzaga35@gmail.com>
2020-10-21 10:17:09 +1100

    deleting branch

--- src/server_channel_test.py
+++ src/server_channel_test.py
@@ -4,6 +4,8 @@ import json
 from test_helpers import url
 from error import AccessError, InputError
 
+VALID_LOGIN_INFO = {"email": "validemail@gmail.com", "password": "123abc!@#"}
+
 def register_new_account(url):
     return requests.post(
         url + "/auth/register",
@@ -16,11 +18,26 @@ def register_new_account(url):
     ).json()
 
 def create_new_channel(url):
+
+    token = auth_login("validemail@gmail.com", "123abc!@#")["token"]
     return requests.post(
         url + "/channels/create",
         json={
-            "token": "validemail@gmail.com",
-            "name": "123abc!@#",
-            "is_public": "Hayden,
+            "token": token,
+            "name": "Hayden",
+            "is_public": True,
         },
     ).json()
+
+def test_channel_join_success(url):
+    requests.delete(url + "clear")
+    user = register_new_account(url)
+    token = requests.post(url + "auth/login", json=VALID_LOGIN_INFO).json()["token"]
+    assert (
+        requests.post(url + "channel_create", json=)
+
+
+    )
+    token = 
+
+    assert register_new_account(url)['code'] == 404
