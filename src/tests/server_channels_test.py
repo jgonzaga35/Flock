c186095bca211daa5d18b5dda19293e88532bc40
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-21 01:21:33 +1100

    channel/list: test where user in multiple channels

--- src/tests/server_channels_test.py
+++ src/tests/server_channels_test.py
@@ -75,8 +75,46 @@ def tests_channels_list_non_admin(url):
     assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
 
 # User in multiple different channels
-def test_channel_list_user_multiple():
-    pass
+def test_channel_list_user_multiple(url):
+    requests.delete(url + "clear")
+    user = http_register_n_users(url, 1)
+
+    # User creates 5 different channels
+    channel_01 = requests.post(
+        url + "channels/create",
+        json={"token": user["token"], "name": "channel_01", "is_public": True},
+    ).json()
+
+    channel_02 = requests.post(
+        url + "channels/create",
+        json={"token": user["token"], "name": "channel_02", "is_public": True},
+    ).json()
+
+    channel_03 = requests.post(
+        url + "channels/create",
+        json={"token": user["token"], "name": "channel_03", "is_public": True},
+    ).json()
+
+    channel_04 = requests.post(
+        url + "channels/create",
+        json={"token": user["token"], "name": "channel_04", "is_public": True},
+    ).json()
+
+    channel_05 = requests.post(
+        url + "channels/create",
+        json={"token": user["token"], "name": "channel_05", "is_public": True},
+    ).json()
+
+    response = requests.get(url + "channels/list", params={"token": user["token"]})
+    assert response.status_code == 200
+    channels_list_dict = response.json()
+    channel_list = [x["channel_id"] for x in channels_list_dict]
+
+    assert channel_01["channel_id"] in channel_list
+    assert channel_02["channel_id"] in channel_list
+    assert channel_03["channel_id"] in channel_list
+    assert channel_04["channel_id"] in channel_list
+    assert channel_05["channel_id"] in channel_list
 
 # Channels list with messages and multiple users in channel
 def tests_channels_list_large(url):
