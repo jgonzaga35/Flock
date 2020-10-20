e651c4d4d4109d53ac21ae0e10f806fd8060b38a
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-10-20 14:36:28 +1100

    add simple test function to get 100% coverage

---
+++ src/tests/test_admin_userpermission_change.py
@@ -0,0 +1,5 @@
+from other import admin_userpermission_change
+
+
+def test_admin_userpermission_change():
+    admin_userpermission_change(-1, -1, -1)
---
+++ src/tests/test_user_all.py
@@ -0,0 +1,5 @@
+from other import users_all
+
+
+def test_user_all():
+    users_all(-1)
