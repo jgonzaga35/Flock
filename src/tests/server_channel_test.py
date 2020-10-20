b668f74fc967f21e31ec0cbe1c0c4d1dc589336f
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-20 22:56:36 +1100

    ensured one channel list is valid

--- src/tests/server_channel_test.py
+++ src/tests/server_channel_test.py
@@ -5,6 +5,7 @@ from test_helpers import url, http_register_n_users
 ###########################################################################
 #                       Tests for channels/list                           #
 ###########################################################################
+# User part of one channel
 def test_channels_list_successful(url):
     requests.delete(url + "clear")
     user = http_register_n_users(url, 1)
@@ -14,5 +15,9 @@ def test_channels_list_successful(url):
     ).json()
 
     response = requests.get(url + "channels/list", params={"token": user["token"]})
-
     assert response.status_code == 200
+    channels_list = response.json()
+    assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
+
+
+# User part of two chanels
