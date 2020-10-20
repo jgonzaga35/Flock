b4498cae87c428e931ea2a016b06d13131d98ed4
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-21 01:13:39 +1100

    test with mock channel with messages and multiple users

--- src/tests/server_channels_test.py
+++ src/tests/server_channels_test.py
@@ -74,17 +74,75 @@ def tests_channels_list_non_admin(url):
     channels_list = response.json()
     assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
 
+# User in multiple different channels
+def test_channel_list_user_multiple():
+    pass
 
 # Channels list with messages and multiple users in channel
-def tests_channels_list_mock_channel(url):
-    pass
+def tests_channels_list_large(url):
     requests.delete(url + "clear")
-    admin = http_register_n_users(url, 1)
+    admin, user01, user02, user03 = http_register_n_users(url, 4)
+
     channel = requests.post(
         url + "channels/create",
         json={"token": admin["token"], "name": "channel_01", "is_public": True},
     ).json()
+
+    # 3 users join the channel
+    r = requests.post(
+        url + "channel/join",
+        json={"token": user01["token"], "channel_id": channel["channel_id"]},
+    )
+    assert r.status_code == 200
+
+    r = requests.post(
+        url + "channel/join",
+        json={"token": user02["token"], "channel_id": channel["channel_id"]},
+    )
+    assert r.status_code == 200
+
+    r = requests.post(
+        url + "channel/join",
+        json={"token": user03["token"], "channel_id": channel["channel_id"]},
+    )
+    assert r.status_code == 200
+
+    # Each user sends a customised message
+    r = requests.post(
+        url + "message/send",
+        json={"token": user01["token"], "channel_id": channel["channel_id"], "message": "user01"},
+    )
+    assert r.status_code == 200
+
+    r = requests.post(
+        url + "message/send",
+        json={"token": user02["token"], "channel_id": channel["channel_id"], "message": "user02"},
+    )
+    assert r.status_code == 200
+
+    r = requests.post(
+        url + "message/send",
+        json={"token": user03["token"], "channel_id": channel["channel_id"], "message": "user03"},
+    )
+    assert r.status_code == 200
+
+    # Ensure each user has this channel in their channel list
     response = requests.get(url + "channels/list", params={"token": admin["token"]})
+    assert response.status_code == 200
+    channels_list = response.json()
+    assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
+
+    response = requests.get(url + "channels/list", params={"token": user01["token"]})
+    assert response.status_code == 200
+    channels_list = response.json()
+    assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
+
+    response = requests.get(url + "channels/list", params={"token": user02["token"]})
+    assert response.status_code == 200
+    channels_list = response.json()
+    assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
+
+    response = requests.get(url + "channels/list", params={"token": user03["token"]})
     assert response.status_code == 200
     channels_list = response.json()
     assert channel["channel_id"] in [x["channel_id"] for x in channels_list]
