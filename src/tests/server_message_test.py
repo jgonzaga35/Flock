7a1ada99c74b5ab4ec21cbe918f4840bf3c7aeaf
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-10-21 14:29:09 +1100

    added assertions throughout tests

--- src/tests/server_message_test.py
+++ src/tests/server_message_test.py
@@ -29,19 +29,23 @@ def test_remove_invalid_user_token(url):
         "name": "channel_01",
         "is_public": True,
     }
-    channel = requests.post(url + "channels/create", json=channel_params).json()
+    response = requests.post(url + "channels/create", json=channel_params)
+    assert response.status_code == 200
+    channel = response.json()
     # User sends a message
     message_params = {
         "token": user["token"],
         "channel_id": channel["channel_id"],
         "message": "test message",
     }
-    message = requests.post(url +  "message/send", json=message_params).json()
+    response = requests.post(url + "message/send", json=message_params)
+    assert response.status_code == 200
+    message = response.json()
+
     message_remove_params = {
         "token": user["token"] + 1,
         "message_id": message["message_id"],
     }
-    
     # Ensure access error raised
     r = requests.delete(url + "message/remove", json=message_remove_params)
     assert r.status_code == 403
@@ -55,7 +59,9 @@ def test_remove_invald_message_id(url):
         "name": "channel_01",
         "is_public": True,
     }
-    channel = requests.post(url + "/channels/create", json=channel_params).json()
+    response = requests.post(url + "channels/create", json=channel_params)
+    assert response.status_code == 200
+    channel = response.json()
     
     # User sends a message
     message_params = {
@@ -63,7 +69,9 @@ def test_remove_invald_message_id(url):
         "channel_id": channel["channel_id"],
         "message": "test message",
     }
-    message = requests.post(url +  "/message/send", json=message_params).json()
+    response = requests.post(url +  "message/send", json=message_params)
+    assert response.status_code == 200
+    message = response.json()
     
     # User tries to edit message with invalid message id
     message_remove_params = {
@@ -104,7 +112,9 @@ def test_remove_unauthorised_user(url):
         "name": "channel_01",
         "is_public": True,
     }
-    channel = requests.post(url + "channels/create", json=channel_params).json()
+    response = requests.post(url + "channels/create", json=channel_params)
+    assert response.status_code == 200
+    channel = response.json()
 
     # User 1 sends a message
     message_params = {
@@ -112,7 +122,9 @@ def test_remove_unauthorised_user(url):
         "channel_id": channel["channel_id"],
         "message": "test message",
     }
-    message = requests.post(url + "message/send", json=message_params).json()
+    response = requests.post(url + "message/send", json=message_params)
+    assert response.status_code == 200
+    message = response.json()
 
     # User 2 tries to remove message from User 1
     message_remove_params = {
@@ -151,22 +163,36 @@ def test_remove_owner(url):
         "name": "channel_01",
         "is_public": True,
     }
-    channel = requests.post(url + "channels/create", json=channel_params).json()
+    response = requests.post(url + "channels/create", json=channel_params)
+    assert response.status_code == 200
+    channel = response.json()
     
     # User 2 sends a message
-    requests.post(url + "channel/join", json={"token": user02["token"], "channel_id": channel["channel_id"]})
+    response = requests.post(
+        url + "channel/join", 
+        json={
+            "token": user02["token"], 
+            "channel_id": channel["channel_id"]
+        }
+    )
+    assert response.status_code == 200
+    
     message_params = {
         "token": user02["token"],
         "channel_id": channel["channel_id"],
         "message": "test message",
     }
-    message = requests.post(url + "message/send", json=message_params).json()
+    response = requests.post(url + "message/send", json=message_params)
+    assert response.status_code == 200
+    message = response.json()
+
     # User01 (admin) removes it
     message_remove_params = {
         "token": user01["token"],
         "message_id": message["message_id"],
     }
-    requests.delete(url + "message/remove", json=message_remove_params)
+    response = requests.delete(url + "message/remove", json=message_remove_params)
+    assert response.status_code == 200
     
     # Confirm that message was removed successfuly
     channel_message_params = {
