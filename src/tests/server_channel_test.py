e75e35f2c8a83dfa5f8f2698661ee11c79d50328
Justin Gonzaga <justingonzaga35@gmail.com>
2020-10-22 12:46:21 +1100

    Copied formatting of original channel_leave tests into server channel_leave tests

--- src/tests/server_channel_test.py
+++ src/tests/server_channel_test.py
@@ -1,33 +1,10 @@
 import requests
 from test_helpers import url, http_register_n_users
+from channel_test import assert_contains_users_id
 
 ###########################################################################
 #                       Tests for channel/leave                           #
 ###########################################################################
-
-# User leaves public channel successfully
-def test_leave_successful(url):
-    requests.delete(url + "clear")
-    user = http_register_n_users(url, 1)
-    response = requests.get(url + "channels/list", params={"token": user["token"]})
-    assert response.json() == []
-
-    channel = requests.post(
-        url + "channels/create",
-        json={"token": user["token"], "name": "channel_01", "is_public": True},
-    ).json()
-
-    requests.post(
-        url + "channel/leave",
-        json={"token": user["token"], "channel_id": channel["channel_id"]},
-    )
-
-    # List of dicts of channels that user is part of
-    response = requests.get(url + "channels/list", params={"token": user["token"]})
-    
-    # Ensure the list is empty
-    assert response.json() == []
-
 
 # User with invalid token tries to leave a channel
 def test_leave_invalid_token(url):
@@ -44,6 +21,41 @@ def test_leave_invalid_token(url):
     )
 
     assert response.status_code == 403
+
+
+# User leaves public channel successfully
+def test_leave_channel_successfully(url):
+    requests.delete(url + "clear")
+    owner, member = http_register_n_users(url, 2)
+    public_channel = requests.post(
+        url + "channels/create",
+        json={"token": owner["token"], "name": "channel_01", "is_public": True},
+    ).json() # Create public channel
+
+    # Member joins the owner's channel so now there is 2 users in channel_01
+    requests.post(
+        url + "channel/join",
+        json={"token": member["token"], "channel_id": public_channel["channel_id"]},
+    )
+    response = requests.get(url + "channel/details", params={"token": owner["token"], "channel_id": public_channel["channel_id"]})
+    channel_details = response.json()
+    expected_members_id = [owner["u_id"], member["u_id"]]
+    assert_contains_users_id(channel_details["all_members"], expected_members_id)
+
+    # Member leaves the channel so there is only the owner in the channel.
+    requests.post(
+        url + "channel/leave",
+        json={"token": member["token"], "channel_id": public_channel["channel_id"]},
+    )
+    response = requests.get(url + "channel/details", params={"token": owner["token"], "channel_id": public_channel["channel_id"]})
+    channel_details = response.json()
+    expected_members_id = [owner["u_id"]]
+    assert_contains_users_id(channel_details["all_members"], expected_members_id)
+
+
+# Need to test that user can no longer see the messages of the channel they have left
+
+# test that owner is no longer part of owner list of channel if they leave
 
 
 # User tries to leave channel with invalid channel id
