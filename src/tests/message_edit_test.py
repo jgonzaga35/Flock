88b48e7a0df41cb2cfde3c1c6eec02e7dfb82807
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-10-23 13:32:36 +1100

    message_edit/test: editing message after leaving channel

--- src/tests/message_edit_test.py
+++ src/tests/message_edit_test.py
@@ -1,9 +1,17 @@
 import pytest
 from test_helpers import register_n_users, assert_contains_users_id
-from channel import channel_join, channel_details, channel_messages, channel_addowner
+from channel import (
+    channel_join,
+    channel_details,
+    channel_messages,
+    channel_addowner,
+    channel_invite,
+    channel_leave,
+)
 from message import message_send, message_edit
 from channels import channels_create
 from other import clear
+from error import AccessError
 
 
 @pytest.mark.parametrize("is_public", (True, False))
@@ -50,3 +58,25 @@ def test_message_edit_admin(is_public):
         channel_messages(usera["token"], channel_id, start=0)["messages"][0]["message"]
         == "admin edit #3"
     )
+
+
+@pytest.mark.parametrize("is_public", (True, False))
+def test_message_edit_out_of_channel(is_public):
+    clear()
+
+    usera, userb = register_n_users(2)
+
+    channel_id = channels_create(usera["token"], "", is_public)["channel_id"]
+    channel_invite(usera["token"], channel_id, userb["u_id"])
+
+    id1 = message_send(usera["token"], channel_id, "message from A!")["message_id"]
+    id2 = message_send(userb["token"], channel_id, "message from B!")["message_id"]
+
+    channel_leave(usera["token"], channel_id)
+    channel_leave(userb["token"], channel_id)
+
+    with pytest.raises(AccessError):
+        message_edit(usera["token"], id1, "edit 1")
+
+    with pytest.raises(AccessError):
+        message_edit(userb["token"], id2, "edit 2")
