142f1269200baf35a6c19bdd8ba2d9b9c075179b
Eric Bai <baixu.must@gmail.com>
2020-10-26 10:14:59 +1100

    server_channels_listall_test: Add comments

--- src/tests/server_channels_listall_test.py
+++ src/tests/server_channels_listall_test.py
@@ -5,7 +5,7 @@ from test_helpers import url, http_register_n_users
 #                       Tests for channels/listall                        #
 ###########################################################################
 
-
+# test channel with invalid token
 def test_channels_listall_invalid_token(url):
 
     requests.delete(url + "clear")
@@ -21,7 +21,7 @@ def test_channels_listall_invalid_token(url):
     response = requests.get(url + "channels/listall", params={"token": invalid_token})
     assert response.status_code == 403
 
-
+# Test when no channel can be list
 def test_channels_listall_empty(url):
     requests.delete(url + "clear")
     user_01 = http_register_n_users(url, 1)
@@ -31,11 +31,12 @@ def test_channels_listall_empty(url):
     assert response.status_code == 200
     assert response.json() == []
 
-
+# Test one user list multiple public channel
 def test_channels_listall_public(url):
     requests.delete(url + "clear")
     user = http_register_n_users(url, 1)
     token = user["token"]
+    # Create two channels by one user
     channel_id_1 = requests.post(
         url + "channels/create",
         json={"token": token, "name": "channel_01", "is_public": True},
@@ -51,10 +52,12 @@ def test_channels_listall_public(url):
     channel_ids = [channel["channel_id"] for channel in response.json()]
     assert channel_ids == [channel_id_1, channel_id_2]
 
+# Test one user list multiple public channel
 def test_channels_listall_private(url):
     requests.delete(url + "clear")
     user = http_register_n_users(url, 1)
     token = user["token"]
+    # Create two channels by one user
     channel_id_1 = requests.post(
         url + "channels/create",
         json={"token": token, "name": "channel_01", "is_public": False},
@@ -70,6 +73,7 @@ def test_channels_listall_private(url):
     channel_ids = [channel["channel_id"] for channel in response.json()]
     assert channel_ids == [channel_id_1, channel_id_2]
 
+# Test multiple users list multiple public channels
 def test_channels_listall_multiple_users_public(url):
     requests.delete(url + "clear")
 
@@ -79,6 +83,8 @@ def test_channels_listall_multiple_users_public(url):
     channel_ids = []
     names = ["private_channel_01", "private_channel_02"]
 
+    # Create a list of tuple which include elements from
+    # names and token, and use it to create two channel
     for name, token in zip(names, tokens):
         channel_id = requests.post(
             url + "channels/create",
@@ -86,10 +92,13 @@ def test_channels_listall_multiple_users_public(url):
         ).json()["channel_id"]
         channel_ids.append(channel_id)
 
+    # Authorised channel is our expectation to the
+    # channels which should be listed.
     authorised_channels = []
     for channel_id, name in zip(channel_ids, names):
         authorised_channels.append({"channel_id": channel_id, "name": name})
 
+    # For any user, channels/listall should list all of channels that exist.
     response01 = requests.get(
         url + "channels/listall", params={"token": user_01["token"]}
     )
@@ -100,6 +109,7 @@ def test_channels_listall_multiple_users_public(url):
     assert response02.json() == authorised_channels
 
 
+# Test multiple users list multiple private channels
 def test_channels_listall_multiple_users_private(url):
     requests.delete(url + "clear")
 
@@ -109,6 +119,9 @@ def test_channels_listall_multiple_users_private(url):
     channel_ids = []
     names = ["private_channel_01", "private_channel_02"]
 
+
+    # Create a list of tuple which include elements from
+    # names and token, and use it to create two channel
     for name, token in zip(names, tokens):
         channel_id = requests.post(
             url + "channels/create",
@@ -116,6 +129,9 @@ def test_channels_listall_multiple_users_private(url):
         ).json()["channel_id"]
         channel_ids.append(channel_id)
 
+
+    # Authorised channel is our expectation to the
+    # channels which should be listed.
     authorised_channels = []
     for channel_id, name in zip(channel_ids, names):
         authorised_channels.append({"channel_id": channel_id, "name": name})
