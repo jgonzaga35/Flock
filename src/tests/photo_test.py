857a59ebd5695be3b91340c19ab85f7ee7eae1f0
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-11-02 18:09:10 +1100

    photo tests: added sample url and crop

--- src/tests/photo_test.py
+++ src/tests/photo_test.py
@@ -1,9 +1,17 @@
 from photo import user_profile_crop_image
+from PIL import Image
+from test_helpers import register_n_users
+from other import clear
+import pytest
+from error import InputError
 
+IMAGE_1_URL = "https://nas-national-prod.s3.amazonaws.com/styles/hero_cover_bird_page/s3/sfw_fixed_01-29-2011-223.jpg?itok=BIR9fBhk"
 def test_crop_simple_crop():
-    pass
+    clear()
+    user = register_n_users(1)
+    user_profile_crop_image(user["token"], IMAGE_1_URL, 1, 1, 1, 1)
 
-def test_crop_coordinates_not_within_bounds():
+def test_crop_dimensions_not_within_bounds():
     pass
 
 def test_crop_image_not_jpg():
