c5cb190dfb81817ae2c05354e86e24d3c6783362
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-11-02 15:51:05 +1100

    crop_img: added basic test skeletons

---
+++ src/tests/photo_test.py
@@ -0,0 +1,10 @@
+from photo import user_profile_crop_image
+
+def test_crop_simple_crop():
+    pass
+
+def test_crop_coordinates_not_within_bounds():
+    pass
+
+def test_crop_image_not_jpg():
+    pass
