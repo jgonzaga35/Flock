bde1b3f9646558d7df3e01ff24a716a1b742c1d0
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-11-03 13:06:55 +1100

    photo: added BytesIO lib for crop func

--- src/photo.py
+++ src/photo.py
@@ -1,6 +1,7 @@
 from auth import auth_get_current_user_id_from_token
 import requests
 from PIL import Image
+from io import BytesIO
 
 # /user/profile/uploadphoto wraps around this function
 def user_profile_crop_image(token, img_url, x_start, y_start, x_end, y_end):
@@ -11,12 +12,31 @@ def user_profile_crop_image(token, img_url, x_start, y_start, x_end, y_end):
     # Ensure token is valid - error checking in function
     user_id = auth_get_current_user_id_from_token(token)
 
-    # Check given dimensions are within original image
-    response = requests.get(image_url)
-    if response.status_code() != 200:
+    response = requests.get(img_url)
+    if response.status_code != 200:
         raise InputError(f"img_url status code is: {response.status_code()}")
 
-    im = Image.open(response.content)
-    print(im)
-    im_dim = im.getbbox()
-    print(im_dim)
+    im = Image.open(BytesIO(response.content))
+    # Get dimensions of given image - x_original_start = x_original_end = 0
+    x_original_end, y_original_end = im.size()
+
+    # Check given dimensions are within original image
+    if (
+        x_start < 0 
+        or x_start > x_original_end
+        or x_end < 0 
+        or x_end > x_original_end
+        or y_start < 0
+        or y_start > y_original_end
+        or y_end < 0 
+        or y_end > y_original_end
+    ):
+        raise InputError(
+            "any of x_start, y_start, x_end, y_end are not within the dimensions of the image at the URL"
+        )
+
+    # TODO: What to do if start > end or vice versa 
+
+    im.crop((x_start, y_start, x_end, y_end))
+    im.show()
+    
--- src/tests/photo_test.py
+++ src/tests/photo_test.py
@@ -6,13 +6,17 @@ import pytest
 from error import InputError
 
 IMAGE_1_URL = "https://nas-national-prod.s3.amazonaws.com/styles/hero_cover_bird_page/s3/sfw_fixed_01-29-2011-223.jpg?itok=BIR9fBhk"
+
+
 def test_crop_simple_crop():
     clear()
     user = register_n_users(1)
     user_profile_crop_image(user["token"], IMAGE_1_URL, 1, 1, 1, 1)
 
+
 def test_crop_dimensions_not_within_bounds():
     pass
+
 
 def test_crop_image_not_jpg():
     pass
