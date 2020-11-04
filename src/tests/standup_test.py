3c62ff4c9114d955eb5332c5fdec83a392f170d8
z5314408 <lplsz2000@gmail.com>
2020-11-04 21:43:05 +1100

    More test completed

--- src/tests/standup_test.py
+++ src/tests/standup_test.py
@@ -33,33 +33,73 @@ def get_time():
 def collect_message(token, channel_id):
     global messages
     messages = channel_messages(token, channel_id, 0)["messages"]
-    
-def check_active():
 
+##################################################################################
+#                          Tests for standup_start                               #
+##################################################################################
 
-def test_start_simple():
+def test_standup_start_simple():
     clear()
+    global messages
+    messages = []
     user1, user2 = register_n_users(2)
 
+    # Create channel and join
     channel = channels_create(user1["token"], "channel1", is_public=True)
     channel_join(user2["token"], channel["channel_id"])
 
+    # Set up the timer
     t1 = Timer(2, collect_message, [user1["token"], channel["channel_id"]])
     t2 = Timer(1, get_time)
     t2.start()
-    standup_start(user1["token"], channel["channel_id"], 1)
+
+    # Start the standup period
+    standup = standup_start(user1["token"], channel["channel_id"], 1)
+    assert standup["is_active"] == True
+    assert standup["time_finish"] == 1
+
+    # Send two standup message
     standup_send(user1["token"], channel["channel_id"], "test1")
     standup_send(user2["token"], channel["channel_id"], "test2")
+    
+    # Assert the messages are empty
+    assert len(messages) == 0
 
     t1.start()
 
+    # Assert the two message has been sent and that the time matches
     assert len(messages) == 2
     for message, i in messages, range(1):
         assert message["time_created"] == time
         assert message["message"] == "test" + str(i)
 
-def test_active_simple():
+def test_standup_start_invalid_channel_id():
     clear()
+    user1 = register_n_users(1)
+
+    invalid_channel_id = -1
+    with pytest.raises(InputError):
+        standup_start(user1["token"], invalid_channel_id, 1)
+
+def test_standup_start_standup_is_running():
+    clear()
+    user1 = register_n_users(1)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    standup_start(user1["token"], channel["channel_id"], 1)
+
+    with pytest.raises(InputError):
+        standup_start(user1["token"], channel["channel_id"], 1)
+
+##################################################################################
+#                         Tests for standup_active                               #
+##################################################################################
+
+def test_standup_active_simple():
+    clear()
+    global messages
+    messages = []
     user1 = register_n_users(1)
     channel = channels_create(user1["token"], "channel1", is_public=True)
 
@@ -68,6 +108,68 @@ def test_active_simple():
 
     time.sleep(2)
     assert standup_active(user1["token"], channel["channel_id"])["is_active"] == False
+
+def test_standup_active_invalid_channel_id():
+    clear()
+    user1 = register_n_users(1)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    standup_start(user1["token"], channel["channel_id"], 1)
+
+    invalid_channel_id = -1
+    with pytest.raises(InputError):
+        standup_active(user1["token"], invalid_channel_id)
+
+##################################################################################
+#                           Tests for standup_send                               #
+##################################################################################
+
+def test_standup_send_invalid_channel_id():
+    clear()
+    user1 = register_n_users(1)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    standup_start(user1["token"], channel["channel_id"], 1)
+
+    invalid_channel_id = -1
+    with pytest.raises(InputError):
+        standup_send(user1["token"], invalid_channel_id, "Test")
+
+def test_standup_send_long_message():
+    clear()
+    user1 = register_n_users(1)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    standup_start(user1["token"], channel["channel_id"], 2)
+
+    standup_send(user1["token"], channel["channel_id"], "a" * 1000)
+
+    with pytest.raises(InputError):
+        standup_send(user1["token"], channel["channel_id"], "a" * 1001)
+
+    with pytest.raises(InputError):
+        standup_send(user1["token"], channel["channel_id"], "a" * 1020)
+
+def test_standup_send_no_active_standup():
+    clear()
+    user1 = register_n_users(1)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    with pytest.raises(InputError):
+        standup_send(user1["token"], channel["channel_id"], "Test")
+
+def test_standup_send_long_message():
+    clear()
+    user1, user2 = register_n_users(2)
+
+    channel = channels_create(user1["token"], "channel1", is_public=True)
+
+    with pytest.raises(AccessError):
+        standup_send(user2["token"], channel["channel_id"], "Test")
 
 
 
