1c94fa14c0f0e4cd4e17bf8850556f71b3af1126
Eric Bai <baixu.must@gmail.com>
2020-11-07 20:06:28 +1100

    message_react: Add all the wrapper tests for the message_react and pass  tests

--- src/tests/server_message_react_test.py
+++ src/tests/server_message_react_test.py
@@ -80,78 +80,170 @@ def test_react_succesfully(url):
     assert user_b["u_id"] in message_b["reacts"][0]["u_ids"]
 
 
-# def test_react_invalid_message_id_in_different_channel():
-#     """
-#     Test the situation where the user trying to react to a message but
-#     not in that channel
-#     """
-#     clear()
-#     user_a, user_b = register_n_users(2)
-#     # user_a create a channel
-#     channels_create(user_a["token"], "public_channel_a", True)["channel_id"]
-#     # user_b create a channel and send message in his own channel
-#     public_channel_id_b = channels_create(user_b["token"], "public_channel_b", True)[
-#         "channel_id"
-#     ]
-#     message_id_b = message_send(
-#         user_b["token"], public_channel_id_b, "I am in channel_b"
-#     )["message_id"]
-#     # user_a should not be able to react the the message in the public_channel_b
-#     with pytest.raises(InputError):
-#         message_react(user_a["token"], message_id_b, 1)
+def test_react_invalid_message_id_in_different_channel(url):
+    """
+    Test the situation where the user trying to react to a message but
+    not in that channel
+    """
+    requests.delete(url + "clear")
+    user_a, user_b = http_register_n_users(url, 2)
+    # user_a create a channel
+    requests.post(
+        url + "channels/create",
+        json={"token": user_a["token"], "name": "public_channel_a", "is_public": True},
+    ).json()["channel_id"]
+    # user_b create a channel and send message in his own channel
+    public_channel_id_b = requests.post(
+        url + "channels/create",
+        json={"token": user_b["token"], "name": "public_channel_b", "is_public": True},
+    ).json()["channel_id"]
+
+    # user_b send a message in his own channel
+    message_id_b = requests.post(
+        url + "message/send",
+        json={
+            "token": user_b["token"],
+            "channel_id": public_channel_id_b,
+            "message": "Nice to see you mate",
+        },
+    ).json()["message_id"]
+    # user_a trying to react the message outside user_a's channel
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": message_id_b,
+            "react_id": 1,
+        },
+    )
+
+    # user_a should not be able to react the the message in the public_channel_b
+    assert response.status_code == 400
 
 
-# def test_react_invalid_message_id_in_channel():
-#     """
-#     Test user trying to unreact to a invalid message id
-#     """
-#     clear()
-#     user_a = register_n_users(1)
-#     channels_create(user_a["token"], "channel_a", True)
-#     invalid_channel_id = -1
-#     with pytest.raises(InputError):
-#         message_react(user_a["token"], invalid_channel_id, 1)
+def test_react_invalid_message_id_in_channel(url):
+    """
+    Test user trying to unreact to a invalid message id
+    """
+    requests.delete(url + "clear")
+    user_a = http_register_n_users(url, 1)
+    requests.post(
+        url + "channels/create",
+        json={"token": user_a["token"], "name": "channel_a", "is_public": True},
+    ).json()["channel_id"]
+    invalid_message_id = -1
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": invalid_message_id,
+            "react_id": 1,
+        },
+    )
+    assert response.status_code == 400
 
 
-# def test_react_invalid_message_id_out_channel():
-#     clear()
-#     user_a, user_b = register_n_users(2)
-#     # user_b create a channel and send message in his own channel
-#     public_channel_id_b = channels_create(user_b["token"], "public_channel_b", True)[
-#         "channel_id"
-#     ]
-#     message_id_b = message_send(
-#         user_b["token"], public_channel_id_b, "I am in channel_b"
-#     )["message_id"]
-#     # user_a should not be able to react the the message in the public_channel_b
-#     with pytest.raises(InputError):
-#         message_react(user_a["token"], message_id_b, 1)
+def test_react_invalid_message_id_out_channel(url):
+    requests.delete(url + "clear")
+    user_a, user_b = http_register_n_users(url, 2)
+    # user_b create a channel and send message in his own channel
+    public_channel_id_b = requests.post(
+        url + "channels/create",
+        json={"token": user_b["token"], "name": "public_channel_b", "is_public": True},
+    ).json()["channel_id"]
+
+    message_id_b = requests.post(
+        url + "message/send",
+        json={
+            "token": user_b["token"],
+            "channel_id": public_channel_id_b,
+            "message": "Nice to see you mate",
+        },
+    ).json()["message_id"]
+
+    # user_a trying to reponse in outside the channel
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": message_id_b,
+            "react_id": 1,
+        },
+    )
+    # user_a should not be able to react the the message in the public_channel_b
+    assert response.status_code == 400
 
 
-# def test_react_invalid_react_id():
-#     clear()
-#     user_a = register_n_users(1)
-#     public_channel_id = channels_create(user_a["token"], "public_channel", True)[
-#         "channel_id"
-#     ]
-#     message_id = message_send(user_a["token"], public_channel_id, "How are you")[
-#         "message_id"
-#     ]
-#     invalid_react_number = -1
-#     with pytest.raises(InputError):
-#         message_react(user_a["token"], message_id, invalid_react_number)
+def test_react_invalid_react_id(url):
+    requests.delete(url + "clear")
+    user_a = http_register_n_users(url, 1)
+
+    # user_a create a channel
+    public_channel_id = requests.post(
+        url + "channels/create",
+        json={"token": user_a["token"], "name": "public_channel", "is_public": True},
+    ).json()["channel_id"]
+
+    # user_a send a message
+    message_id = requests.post(
+        url + "message/send",
+        json={
+            "token": user_a["token"],
+            "channel_id": public_channel_id,
+            "message": "How are you",
+        },
+    ).json()["message_id"]
+    invalid_react_number = -1
+
+    # user_a react the the message using a invalid message_id
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": message_id,
+            "react_id": invalid_react_number,
+        },
+    )
+    assert response.status_code == 400
 
 
-# def test_react_duplicate_react():
-#     clear()
-#     user_a = register_n_users(1)
-#     public_channel_id = channels_create(user_a["token"], "public_channel", True)[
-#         "channel_id"
-#     ]
-#     message_id = message_send(user_a["token"], public_channel_id, "How are you")[
-#         "message_id"
-#     ]
-#     message_react(user_a["token"], message_id, 1)
-#     # React to the same message twice
-#     with pytest.raises(InputError):
-#         message_react(user_a["token"], message_id, 1)
+def test_react_duplicate_react(url):
+    requests.delete(url + "clear")
+    user_a = http_register_n_users(url, 1)
+
+    # user_a create a channel
+    public_channel_id = requests.post(
+        url + "channels/create",
+        json={"token": user_a["token"], "name": "public_channel", "is_public": True},
+    ).json()["channel_id"]
+
+    message_id = requests.post(
+        url + "message/send",
+        json={
+            "token": user_a["token"],
+            "channel_id": public_channel_id,
+            "message": "How are you",
+        },
+    ).json()["message_id"]
+
+    # React for the first time
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": message_id,
+            "react_id": 1,
+        },
+    )
+    assert response.status_code == 200
+
+    # React to the same message twice
+    response = requests.post(
+        url + "message/react",
+        json={
+            "token": user_a["token"],
+            "message_id": message_id,
+            "react_id": 1,
+        },
+    )
+    assert response.status_code == 400
