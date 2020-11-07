f8d419dac53d7a34b922ba2d096da828c0c83ee0
Justin Gonzaga <justingonzaga35@gmail.com>
2020-11-07 15:34:58 +1100

    implemented auth_passwordreset functions

---
+++ src/auth_passwordreset.py
@@ -0,0 +1,120 @@
+import re
+from database import database
+from error import InputError, AccessError
+import jwt
+from hashlib import sha256
+import random
+import string
+from auth import auth_get_user_data_from_id, auth_register, encrypt
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SENDER_EMAIL = "comp1531mangoteam2@gmail.com"
+PASSWORD = "mangosquad123"
+
+# Generates a random alphanumeric string of specified length.
+def generate_reset_code(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    code = "".join(random.choice(letters_and_digits) for i in range(length))
+    return code
+
+
+# Sends email containing password reset code to receiver_email.
+def send_passwordreset_request_email(receiver_email, user, reset_code):
+
+    # Construct message
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Password Reset Request"
+    message["From"] = SENDER_EMAIL
+    message["To"] = receiver_email
+
+    text = """\
+    Hello {name},
+
+    Please use the following code to reset your password.
+    Code: {reset_code}""".format(
+        name=user["first_name"], reset_code=reset_code
+    )
+
+    text = MIMEText(text, "plain")
+    message.attach(text)
+
+    # Send message
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(SENDER_EMAIL, PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_email, message.as_string())
+
+
+# Helper to get all user data from their email
+def auth_get_user_data_from_email(email):
+    for user in database["users"].values():
+        if user["email"] == email:
+            return user
+    raise InputError("Email does not correspond to registered user")
+
+
+# Sends user an email with a code allowing them to reset their password
+def auth_passwordreset_request(email):
+    user = auth_get_user_data_from_email(email)
+    previously_resetted = False
+
+    # Generate random alphanumeric code of length 10
+    reset_code = generate_reset_code(10)
+
+    # If user has resetted their password before, just change existing reset_code.
+    for user_reset_code in database["reset_codes"].values():
+        if user["id"] == user_reset_code["u_id"]:
+            user_reset_code["reset_code"] = reset_code
+            previously_resetted = True
+            break
+
+    # If user has not resetted their password before, add to database.
+    # Store reset code in index corresponding to the appropriate user_id
+    if previously_resetted == False:
+        user_reset_code = {
+            "u_id": user["id"],
+            "reset_code": reset_code,
+        }
+        index = database["reset_codes_head"]
+        database["reset_codes_head"] += 1
+
+        database["reset_codes"][index] = user_reset_code
+
+    # Send password reset email to user
+    send_passwordreset_request_email(email, user, reset_code)
+
+    return {}
+
+
+# Helper function to get user id from reset code
+def get_u_id_from_reset_code(reset_code):
+    if reset_code == "invalid":
+        raise InputError("Invalid reset code")
+
+    for user_reset_code in database["reset_codes"].values():
+        if reset_code == user_reset_code["reset_code"]:
+            return user_reset_code["u_id"]
+    raise InputError("Invalid reset code")
+
+
+# Resets password with a new password given the correct reset code
+def auth_passwordreset_reset(reset_code, new_password):
+    # Check if new password is valid.
+    if len(new_password) < 6:
+        raise InputError("Password is too short.")
+
+    # Check if reset_code is valid.
+    u_id = get_u_id_from_reset_code(reset_code)
+
+    # Reset password
+    database["users"][u_id]["password"] = encrypt(new_password)
+
+    # Delete reset code
+    for code in database["reset_codes"].values():
+        if code["reset_code"] == reset_code:
+            code["reset_code"] = "invalid"
+            break
+
+    return {}
