ac057c510718d0dcc4dbf4645e5b5ef9fe70b88a
Prayag Rawat <z5312819@ad.unsw.edu.au>
2020-11-10 20:29:25 +1100

    message/pin: cleaned up tests

--- src/tests/server_message_pin_test.py
+++ src/tests/server_message_pin_test.py
@@ -39,10 +39,8 @@ def create_channel(url):
     return [url, user, channel, message]
 
 
-def test_message_pin_public_simple(create_channel):
-    url, user, channel, message = create_channel
-
-    # Return messages in channel
+# Returns the first messsage in channel id
+def return_first_message_in_channel(url, user, channel):
     response = requests.get(
         url + "channel/messages",
         params={
@@ -53,29 +51,24 @@ def test_message_pin_public_simple(create_channel):
     )
     assert response.status_code == 200
     channel_msg = response.json()
+    return channel_msg["messages"][0]
 
-    # Ensure message is not pinned
-    assert channel_msg["messages"][0]["is_pinned"] == False
 
+def test_message_pin_public_simple(create_channel):
+    url, user, channel, message = create_channel
+
+    # Return messages in channel, and ensure message not pinned
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == False
+
+    # Pin message
     response = requests.post(
         url + "message/pin",
         json={"token": user["token"], "message_id": message["message_id"]},
     )
-
     assert response.status_code == 200
-    # Return messages in channel
-    response = requests.get(
-        url + "channel/messages",
-        params={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "start": 0,
-        },
-    )
-    assert response.status_code == 200
-    channel_msg = response.json()
-
-    assert channel_msg["messages"][0]["is_pinned"] == True
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == True
 
 
 def test_message_pin_private_simple(url):
@@ -106,119 +99,54 @@ def test_message_pin_private_simple(url):
     assert response.status_code == 200
     message = response.json()
 
-    # Return messages in channel
-    response = requests.get(
-        url + "channel/messages",
-        params={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "start": 0,
-        },
-    )
-    assert response.status_code == 200
-    channel_msg = response.json()
+    # Return messages in channel, and ensure message not pinned
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == False
 
-    # Ensure message is not pinned
-    assert channel_msg["messages"][0]["is_pinned"] == False
-
+    # Pin message
     response = requests.post(
         url + "message/pin",
         json={"token": user["token"], "message_id": message["message_id"]},
     )
-
     assert response.status_code == 200
 
     # Return updated messages in channel
-    response = requests.get(
-        url + "channel/messages",
-        params={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "start": 0,
-        },
-    )
-    assert response.status_code == 200
-    channel_msg = response.json()
-
-    assert channel_msg["messages"][0]["is_pinned"] == True
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == True
 
 
-def test_message_pin_invalid_msesage_id(url):
-    requests.delete(url + "clear")
-    user = http_register_n_users(url, 1)
+def test_message_pin_invalid_message_id(create_channel):
+    url, user, channel, message = create_channel
 
-    # Create a channel
-    response = requests.post(
-        url + "channels/create",
-        json={
-            "token": user["token"],
-            "name": "channel_01",
-            "is_public": False,
-        },
-    )
-    assert response.status_code == 200
-    channel = response.json()
+    # Ensure message not pinned
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == False
 
-    # User sends a message
-    response = requests.post(
-        url + "message/send",
-        json={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "message": "test message",
-        },
-    )
-    assert response.status_code == 200
-    message = response.json()
-
-    # Return messages in channel
-    response = requests.get(
-        url + "channel/messages",
-        params={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "start": 0,
-        },
-    )
-    assert response.status_code == 200
-    channel_msg = response.json()
-
-    # Ensure message is not pinned
-    assert channel_msg["messages"][0]["is_pinned"] == False
-
+    # Attempt to pin messaeg with invalid message_id
     response = requests.post(
         url + "message/pin",
         json={"token": user["token"], "message_id": INVALID_MESSAGE_ID},
     )
-
     assert response.status_code == 400
 
 
 def test_message_already_pinned(create_channel):
     url, user, channel, message = create_channel
 
-    # Return messages in channel
-    response = requests.get(
-        url + "channel/messages",
-        params={
-            "token": user["token"],
-            "channel_id": channel["channel_id"],
-            "start": 0,
-        },
-    )
-    assert response.status_code == 200
-    channel_msg = response.json()
+    # Ensure message not pinned
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == False
 
-    # Ensure message is not pinned, and pin message
-    assert channel_msg["messages"][0]["is_pinned"] == False
+    # Pin message
     response = requests.post(
         url + "message/pin",
         json={"token": user["token"], "message_id": message["message_id"]},
     )
     assert response.status_code == 200
+    channel_msg = return_first_message_in_channel(url, user, channel)
+    assert channel_msg["is_pinned"] == True
 
     # Attempt to pin a message that is already pinned
-    assert channel_msg["messages"][0]["is_pinned"] == False
     response = requests.post(
         url + "message/pin",
         json={"token": user["token"], "message_id": message["message_id"]},
