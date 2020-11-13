04c6919ea1b2aeb295f255a92f1aa1e47c12c059
Mathieu PATUREL <m.paturel@student.unsw.edu.au>
2020-11-13 15:57:22 +1100

    sendlater: use integer times

--- src/tests/message_sendlater_test.py
+++ src/tests/message_sendlater_test.py
@@ -9,7 +9,7 @@ from test_helpers import register_n_users
 
 # bump this up if you are running on a potato computer. It will scale delays,
 # but tests will *take* longer (not *run* slower)
-DELAY_SCALE = 1
+DELAY_SCALE = 1  # it should be an integer
 
 ERR_TOO_SLOW = "potato computer detected. Bump up DELAY_SCALE, or try to re-run"
 
@@ -54,16 +54,16 @@ def test_sendlater_single():
     channel_id = channels_create(user["token"], "name", is_public=True)["channel_id"]
 
     # send message with delay
-    send_at = time.time() + 0.5 * DELAY_SCALE
+    send_at = int(round(time.time() + 1 * DELAY_SCALE))
     sendlater(user["token"], channel_id, "message", send_at)
 
     # make sure there are now messages
     messages = channel_messages(user["token"], channel_id, start=0)
-    assert time.time() < send_at, ERR_TOO_SLOW
+    assert int(round(time.time())) < send_at, ERR_TOO_SLOW
     assert len(messages["messages"]) == 0
 
     # wait for the message to be sent
-    time.sleep(send_at - time.time() + 0.5 * DELAY_SCALE)
+    time.sleep(2)
 
     # check that the message is there
     messages = channel_messages(user["token"], channel_id, start=0)
@@ -72,7 +72,7 @@ def test_sendlater_single():
 
     # if this fails, there has been a time difference of more than half a second
     # time.time() is a float, and floats shouldn't be compared with ==
-    assert messages["messages"][0]["time_created"] - send_at < 0.5
+    assert messages["messages"][0]["time_created"] - send_at <= 1
 
 
 def test_sendlater_backwards():
@@ -84,30 +84,30 @@ def test_sendlater_backwards():
     channel_id = channels_create(usera["token"], "name", is_public=True)["channel_id"]
     channel_join(userb["token"], channel_id)
 
-    send_at_a = time.time() + 1 * DELAY_SCALE
+    send_at_a = int(round(time.time() + 3 * DELAY_SCALE))
     # message b will be sent *before* message a
-    send_at_b = send_at_a - 0.5 * DELAY_SCALE
+    send_at_b = int(round(time.time() + 1 * DELAY_SCALE))
 
     sendlater(usera["token"], channel_id, "message a", send_at_a)
     sendlater(userb["token"], channel_id, "message b", send_at_b)
 
     messages = channel_messages(usera["token"], channel_id, start=0)
-    assert time.time() < send_at_b, ERR_TOO_SLOW
+    assert int(round(time.time())) < send_at_b, ERR_TOO_SLOW
     assert len(messages["messages"]) == 0
 
     # wait for the first message (message B) to be sent
-    time.sleep(send_at_b - time.time() + 0.25 * DELAY_SCALE)
+    time.sleep(2)
 
     messages = channel_messages(usera["token"], channel_id, start=0)
-    assert time.time() < send_at_a, ERR_TOO_SLOW
+    assert int(round(time.time())) < send_at_a, ERR_TOO_SLOW
     assert len(messages["messages"]) == 1
     assert messages["messages"][0]["message"] == "message b"
-    assert (messages["messages"][0]["time_created"] - send_at_b) < 0.5
+    assert (messages["messages"][0]["time_created"] - send_at_b) <= 1
 
     # wait for the second message (message A) to be sent
-    time.sleep(send_at_a - time.time() + 0.25 * DELAY_SCALE)
+    time.sleep(2)
 
     messages = channel_messages(userb["token"], channel_id, start=0)
     assert len(messages["messages"]) == 2
     assert messages["messages"][0]["message"] == "message a"
-    assert (messages["messages"][0]["time_created"] - send_at_a) < 0.5
+    assert (messages["messages"][0]["time_created"] - send_at_a) <= 1
